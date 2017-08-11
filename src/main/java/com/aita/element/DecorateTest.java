6e227825204a460f83827c8f49a2fd5149a8bc94
wansong <wansong@firstshare.cn>
2017-08-11 09:19:52 +0800

    init project

---
+++ src/main/java/com/aita/element/DecorateTest.java
@@ -0,0 +1,11 @@
+package com.aita.element;
+
+/**
+ * Created by Aaron on 10/08/2017.
+ */
+public class DecorateTest {
+    private String name;
+    private String age;
+
+
+}
---
+++ src/main/java/com/aita/element/Person.java
@@ -0,0 +1,10 @@
+package com.aita.element;
+
+/**
+ * Created by Aaron on 10/08/2017.
+ */
+public class Person {
+    protected String name;
+    String age;
+    private String xx;
+}
