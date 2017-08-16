9c61cfd0144589824d88ed47d368c7120f3f0e7b
wansong <wansong@firstshare.cn>
2017-08-17 06:18:01 +0800

    add singleton

---
+++ oop/src/main/java/com/aita/oop/classoop/Some.java
@@ -0,0 +1,19 @@
+package com.aita.oop.classoop;
+
+/**
+ * @author 万松(Aaron)
+ * @since 5.7
+ */
+public class Some {
+
+    private static Some some;
+
+    public Some() {
+    }
+    public static Some create(){
+        if(some==null){
+            some=new Some();
+        }
+        return some;
+    }
+}
