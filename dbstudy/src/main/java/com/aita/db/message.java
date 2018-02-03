c7629586bae99b476b015beed32083780de82aba
zhenglina <zhenglina2011@163.com>
2018-02-03 22:56:02 +0800

    message

---
+++ dbstudy/src/main/java/com/aita/db/message.java
@@ -0,0 +1,56 @@
+package com.aita.db;
+
+/**
+ * Created by Aita on 18/2/3.
+ */
+
+import java.io.Serializable;
+
+package cc.openhome;
+
+
+public class message implements Serializable {
+    private Long id;
+    private String name;
+    private String email;
+    private String msg;
+
+    public Message() {}
+    public Message(String name, String email, String msg){
+        this.name = name;
+        this.email = email;
+        this.msg = msg;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public Long getId(){
+        return id;
+    }
+
+    public void setId(Long id){
+        this.id = id;
+    }
+
+    public  String getMsg(){
+        return msg;
+    }
+
+    public void setMsg(String msg){
+        this.msg = msg;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+}
