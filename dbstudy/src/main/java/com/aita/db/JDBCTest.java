b62ef121872c0ffa99cc012519ad9e9e6603eb56
wansong <wansong@firstshare.cn>
2018-02-03 11:49:48 +0800

    update

--- dbstudy/src/main/java/com/aita/db/JDBCTest.java
+++ dbstudy/src/main/java/com/aita/db/JDBCTest.java
@@ -8,11 +8,11 @@ import java.sql.*;
 public class JDBCTest {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
         Class.forName("com.mysql.jdbc.Driver");
-        Connection connection = DriverManager.getConnection("");
-        PreparedStatement pstm = connection.prepareStatement("");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/book_manager", "root", "123456");
+        PreparedStatement pstm = connection.prepareStatement("select * from book");
         ResultSet result = pstm.executeQuery();
         while (result.next()){
-            result.getString(1);
+            System.out.println(result.getNString("name"));
         }
 
     }
