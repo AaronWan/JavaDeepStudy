bbf497879110744d4060d14083731189ea737528
zhenglina <zhenglina2011@163.com>
2018-02-03 22:55:34 +0800

    jdbc update

--- dbstudy/src/main/java/com/aita/db/JDBCTest.java
+++ dbstudy/src/main/java/com/aita/db/JDBCTest.java
@@ -10,7 +10,15 @@ public class JDBCTest {
         Class.forName("com.mysql.jdbc.Driver");
         Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/book_manager", "root", "123456");
         PreparedStatement pstm = connection.prepareStatement("select * from book");
-        ResultSet result = pstm.executeQuery();
+
+        Statement statement = connection.createStatement();
+            String sql1 = String.format("insert into member(name, mail, passwd) values('zheng', 'zhenglina2011@163.com', '12345678')");
+            String sql2 = String.format("insert into member(name, mail, passwd) values('lina', 'lina@163.com', '12345678')");
+            String sql3 = String.format("insert into member(name, mail, passwd) values('Rita', 'aita0908@163.com', '12345678')");
+
+        statement.executeUpdate(sql3);
+        ResultSet result = pstm.executeQuery("select * from member");
+
         while (result.next()){
             System.out.println(result.getNString("name"));
         }
